b363337c6223378af6fdda0adccc6a88e36a9cfc
David Raleigh <davidraleigh@gmail.com>
2015-05-28 18:14:53 +0200

    character list

--- routes/bard.js
+++ routes/bard.js
@@ -5,6 +5,7 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+var url = require('url');
 
 var mongoURL = 'mongodb://localhost:27017/bard';
 
@@ -30,6 +31,34 @@ router.get('/playTitles', function(req, res, next) {
         });
     });
 });
+
+//All's%20Well%20That%20Ends%20Well
+router.get('/playCharacters', function(req, res, next) {
+    // get the filter from the url.
+    var result = url.parse(req.url, true);
+    // TODO handle the case where an incorrect formated url is passed.
+    MongoClient.connect(mongoURL, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+        res.setHeader('Content-Type', 'application/json');
+        var collection = db.collection('playOverview');
+        collection.findOne(result.query, {fields:{'characters':true, '_id':false}}, function(err, doc) {
+            if (err || doc === null) {
+                // TODO return error
+            }
+            //var arrayOfTitles = doc.map(function(item) {
+            //    return item.playTitle;
+            //});
+            if (doc != null) {
+                res.send(JSON.stringify(doc));
+            } else {
+                // TODO error return
+            }
+        });
+    });
+});
+
+
 
 module.exports = router;
 
