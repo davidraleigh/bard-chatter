2b2d593a35cdfa0151d706e568174365c8f64497
David Raleigh <davidraleigh@gmail.com>
2015-05-28 16:05:47 +0200

    added database and query for titles

--- routes/bard.js
+++ routes/bard.js
@@ -3,14 +3,32 @@
  */
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
-var url = 'mongodb://localhost:27017/bard';
+var mongoURL = 'mongodb://localhost:27017/bard';
 
 /* GET all shakespeare play titles listing. */
-router.get('/playList', function(req, res, next) {
-    var playList = {playname : 'Othello'};
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(playList));
+router.get('/playTitles', function(req, res, next) {
+    MongoClient.connect(mongoURL, function(err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+        res.setHeader('Content-Type', 'application/json');
+        var collection = db.collection('playOverview');
+        collection.find({}, {'playTitle':true, '_id':false}).toArray(function(err, doc) {
+            if (err || doc === null) {
+                // TODO return error
+            }
+            var arrayOfTitles = doc.map(function(item) {
+               return item.playTitle;
+            });
+            if (doc != null) {
+                res.send(JSON.stringify(arrayOfTitles));
+            } else {
+                // TODO error return
+            }
+        });
+    });
 });
 
 module.exports = router;
